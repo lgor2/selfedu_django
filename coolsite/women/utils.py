9056feaab36052e45e7cee0dd9e40806f412c65c
lgor <lgor.2.lgor.2.lgor@gmail.com>
2021-12-12 22:22:07 +0200

    lesson 22

--- coolsite/women/utils.py
+++ coolsite/women/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.core.cache import cache
 
 from .models import *
 
@@ -14,7 +15,11 @@ class DataMixin:
 
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('women'))
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            cache.set('cats', cats, 60)
+
         context['cats'] = cats
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
